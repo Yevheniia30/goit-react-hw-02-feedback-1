b71dd39121a4434d4e98aaad7b04ad58eb6bc387
Evgeniya3010 <susskaya86@gmail.com>
2021-02-25 03:19:53 +0200

    step 2 done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
   // ф-ция для положительных отзывов
   handleGood = () => {
     this.setState(prevState => {
@@ -38,6 +39,11 @@ class App extends Component {
   };
 
   render() {
+    const values = Object.values(this.state);
+    const goodFeedbacks = values[0];
+    const allFeedbacks = values[0] + values[1] + values[2];
+    const persentage = Math.round((goodFeedbacks / allFeedbacks) * 100);
+
     const { good, neutral, bad } = this.state;
     return (
       <div className="App-main">
@@ -73,6 +79,10 @@ class App extends Component {
             <p className={s.feedbackQuantity}>Good: {good}</p>
             <p className={s.feedbackQuantity}>Neutral: {neutral} </p>
             <p className={s.feedbackQuantity}>Bad: {bad}</p>
+            <p className={s.feedbackQuantity}>Total: {allFeedbacks}</p>
+            <p className={s.feedbackQuantity}>
+              Positive feedback: {persentage}%
+            </p>
           </div>
           {/* <FeedbackOptions
             onGood={this.handleGood}
