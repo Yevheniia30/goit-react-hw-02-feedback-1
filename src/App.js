ee2ff444e1280f9e9427e314fc99e4ca1a61ec25
Evgeniya3010 <susskaya86@gmail.com>
2021-03-15 05:39:34 +0200

    feedbackoptions fixed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import FeedbackOptions from './components/FeedbackOptions';
 import Section from './components/Section';
 import Notification from './components/Notification';
 
+const feedback_options = ['good', 'neutral', 'bad'];
+
 class App extends Component {
   state = {
     good: 0,
@@ -12,67 +14,99 @@ class App extends Component {
     bad: 0,
   };
 
-  // ф-ция для положительных отзывов
-  handleGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
+  handleFeedbacks = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
   };
-
-  // ф-ция для нейтральніх отзывов
-  handleNeutral = () => {
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
-
-  // ф-ция для отрицательных отзывов
-  handleBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  };
-
-  // ф-ция показа статистики только при первом нажатии кнопки
-  onLeaveFeedback = () => {};
 
   render() {
-    const values = Object.values(this.state);
-    const goodFeedbacks = values[0];
-    const total = values.reduce((acc, value) => acc + value);
-    const positivePercentage = Math.round((goodFeedbacks / total) * 100);
-
-    const { good, neutral, bad } = this.state;
     return (
       <div className={s.App}>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            handleGood={this.handleGood}
-            handleBad={this.handleBad}
-            handleNeutral={this.handleNeutral}
-            onLeaveFeedback={this.onLeaveFeedback}
+            options={feedback_options}
+            onLeaveFeedBack={this.handleFeedbacks}
           />
         </Section>
 
         <Section title="Statistics">
           <Notification message="No feedback given" />
+
           <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
+          // good={good}
+          // neutral={neutral}
+          // bad={bad}
+          // total={total}
+          // positivePercentage={positivePercentage}
           ></Statistics>
         </Section>
       </div>
     );
   }
 }
+
+// ф-ция для положительных отзывов
+// handleGood = () => {
+//   this.setState(prevState => {
+//     return {
+//       good: prevState.good + 1,
+//     };
+//   });
+// };
+
+// ф-ция для нейтральніх отзывов
+// handleNeutral = () => {
+//   this.setState(prevState => {
+//     return {
+//       neutral: prevState.neutral + 1,
+//     };
+//   });
+// };
+
+// ф-ция для отрицательных отзывов
+// handleBad = () => {
+//   this.setState(prevState => {
+//     return {
+//       bad: prevState.bad + 1,
+//     };
+//   });
+// };
+
+// ф-ция показа статистики только при первом нажатии кнопки
+// onLeaveFeedback = () => {};
+
+// render() {
+//   const values = Object.values(this.state);
+//   const goodFeedbacks = values[0];
+//   const total = values.reduce((acc, value) => acc + value);
+//   const positivePercentage = Math.round((goodFeedbacks / total) * 100);
+
+//   const { good, neutral, bad } = this.state;
+//   return (
+//     <div className={s.App}>
+//       <Section title="Please leave feedback">
+//         <FeedbackOptions
+//           handleGood={this.handleGood}
+//           handleBad={this.handleBad}
+//           handleNeutral={this.handleNeutral}
+//           onLeaveFeedback={this.onLeaveFeedback}
+//         />
+//       </Section>
+
+//       <Section title="Statistics">
+//         <Notification message="No feedback given" />
+//         <Statistics
+//           good={good}
+//           neutral={neutral}
+//           bad={bad}
+//           total={total}
+//           positivePercentage={positivePercentage}
+//         ></Statistics>
+//       </Section>
+//     </div>
+//   );
+// }
+// }
 
 export default App;
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,44 +1,20 @@
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 // когда передаем пропы функций, негласный стандарт называть их On...
-const FeedbackOptions = ({
-  handleGood,
-  handleNeutral,
-  handleBad,
-  onLeaveFeedback,
-}) => {
+const FeedbackOptions = ({ options, onleaveFeedback }) => {
   return (
-    <div className={s.feedbackOptions}>
-      <button
-        type="button"
-        className={s.btn + ' ' + s.goodBtn}
-        onClick={handleGood}
-      >
-        Good
-      </button>
-      <button
-        type="button"
-        className={s.btn + ' ' + s.neutralBtn}
-        onClick={handleNeutral}
-      >
-        <img src="" alt="" />
-        Neutral
-      </button>
-      <button
-        type="button"
-        className={s.btn + ' ' + s.badBtn}
-        onClick={handleBad}
-      >
-        Bad
-      </button>
+    <div>
+      {options.map(option => (
+        <button key={option} className={s.btn} onClick={onleaveFeedback}>
+          {option}
+        </button>
+      ))}
     </div>
   );
 };
 
 FeedbackOptions.propTypes = {
-  handleGood: PropTypes.func.isRequired,
-  handleNeutral: PropTypes.func.isRequired,
-  handleBad: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
 };
 
 export default FeedbackOptions;
